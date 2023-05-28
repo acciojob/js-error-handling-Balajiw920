d265f6a5447e77b683c18853352cbb69127ba607
Balajiw920 <wbalaji2021@gmail.com>
2023-05-28 16:56:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your code here
 class OutOfRangeError extends Error {
   constructor() {
     super();
@@ -11,24 +10,29 @@ class InvalidExprError extends Error {
   constructor() {
     super();
     this.name = this.constructor.name;
-    this.message = "Expression should not have an invalid combination of expression";
+    this.message = "Expression should not have an invalid combination of operators";
   }
 }
 
 function evalString(expression) {
   try {
+    // Throw error if there is an invalid character
+    if (!/^[\d\s\+\-\*\/]+$/.test(expression)) {
+      throw new OutOfRangeError();
+    }
+
     // Throw error if there is a combination of operators like ++, /+, etc.
     if (/[\+\-\/\*]{2,}/.test(expression)) {
       throw new InvalidExprError();
     }
 
-    // Throw Syntax Error if Expression starts with +, /, * operator
-    if (/^[\+\/\*]/.test(expression)) {
+    // Throw Syntax Error if Expression starts with an invalid operator
+    if (/^[\*\/]/.test(expression)) {
       throw new SyntaxError("Expression should not start with an invalid operator");
     }
 
-    // Throw Syntax Error if Expression ends with +, /, *, - operator
-    if (/[\+\/\*\-]$/.test(expression)) {
+    // Throw Syntax Error if Expression ends with an invalid operator
+    if (/[\+\-\*\/]$/.test(expression)) {
       throw new SyntaxError("Expression should not end with an invalid operator");
     }
 
@@ -44,15 +48,23 @@ function evalString(expression) {
   }
 }
 
-try {
-  // Test your expression here
-  const expression = "5 + 10 / 2";
-  const result = evalString(expression);
-  console.log(result);
-} catch (error) {
-  if (error instanceof OutOfRangeError || error instanceof InvalidExprError) {
-    console.error(error.message);
-  } else {
-    console.error("An unexpected error occurred:", error);
+const expressionInput = document.getElementById("expression");
+const resultDiv = document.getElementById("result");
+
+expressionInput.addEventListener("input", () => {
+  const expression = expressionInput.value.trim();
+  resultDiv.textContent = "";
+  try {
+    if (expression) {
+      const result = evalString(expression);
+      resultDiv.textContent = `Result: ${result}`;
+    }
+  } catch (error) {
+    if (error instanceof OutOfRangeError || error instanceof InvalidExprError) {
+      resultDiv.textContent = error.message;
+    } else {
+      resultDiv.textContent = "An unexpected error occurred.";
+      console.error(error);
+    }
   }
-}
+});
